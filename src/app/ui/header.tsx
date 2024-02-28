898e23c39337961b1fcf0befe3a4db6595e683ce
Mahadev Maitri <mahadevmaitri@gmail.com>
2024-02-28 08:50:32 -0500

    initial layout

---
+++ src/app/ui/header.tsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+  return (
+    <div>
+      <div className="text-black min-h-16 flex place-content-center place-items-center">
+        <div className="text-4xl font-serif font-semibold">Wordle App</div>
+      </div>
+      <hr className="mx-auto mb-4" />
+    </div>
+  );
+}
---
+++ src/app/ui/wordle.tsx
@@ -0,0 +1,8 @@
+
+export default function Wordle() {
+  return (
+    <div>
+      <h1 className="text-black">Wordle App</h1>
+    </div>
+  );
+}
